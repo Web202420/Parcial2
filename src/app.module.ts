b0b3b831185ebd6bf391ea2a98a167aedb8b8067
wareval0 <w.arevalo@uniandes.edu.co>
2024-11-28 10:36:40 -0500

    Inicializacion de TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,26 @@ import { AppService } from './app.service';
 import { UsuarioModule } from './usuario/usuario.module';
 import { ClaseModule } from './clase/clase.module';
 import { BonoModule } from './bono/bono.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsuarioModule, ClaseModule, BonoModule],
+  imports: [
+    UsuarioModule,
+    ClaseModule,
+    BonoModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'pre_parcial',
+      entities: [UsuarioModule, ClaseModule, BonoModule],
+      dropSchema: true,
+      synchronize: true,
+      keepConnectionAlive: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
